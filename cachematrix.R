6039f35dccbb2cffa391ba77412a4be56bb90402
Gabriel-Simmonds <gf_simmonds@hotmail.com>
2014-04-23 22:14:11 +0100

    Two functions to show matrix inverse

--- cachematrix.R
+++ cachematrix.R
@@ -4,10 +4,10 @@
 
 ## This first function will create a special "matrix", which is a list 
 ## containing a function to:-
-## 1) set the value of the matrix
-## 2) get the value of the matrix
-## 3) set the value of the inverse matrix using the solve() function
-## 4) get the value of the inverse matrix using the solve() function
+## 1) set matrix value
+## 2) get matrix value
+## 3) set inverse matrix value using the solve() function
+## 4) get inverse matrix value using the solve() function
 makeCacheMatrix <- function(x = matrix()) {
 	s <- NULL
 	set <- function(y) {
@@ -26,8 +26,8 @@ makeCacheMatrix <- function(x = matrix()) {
 ## This second function calculates the inverse of the special "matrix" created 
 ## with the makeCacheMatrix function. First it checks to see if the
 ## inverse has already been calculated. If so, it gets the inverse from the 
-## cache and no calculation is necessary. Otherwise, it calculates the inverse 
-## of the matrix and assigns the value to the cache via the setSolve function.
+## cache and no calculation is needed. Otherwise, it calculates inverse of 
+## the matrix and assigns value to the cache via the setSolve function.
 cacheSolve <- function(x, ...) {
 	s <- x$getSolve()
 	if(!is.null(s)) {
